596bf460732ba01564abf2e53e1256c5c29ecbc8
Luigi Renzo Geralde <152177012+lmgeralde@users.noreply.github.com>
2025-09-01 15:25:07 +0800

    Update AdminPasswordSeeder.php

--- database/seeders/AdminPasswordSeeder.php
+++ database/seeders/AdminPasswordSeeder.php
@@ -2,16 +2,16 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AdminPasswordSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        User::where('email', 'admin@valesresort.com')->update([
+            'password' => Hash::make('admin123')
+        ]);
     }
 }
